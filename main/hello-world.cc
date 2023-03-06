28099ae01aeb2837f5154ef5279fd43dd0e15c4d
Mohamad <m.bassel@t3k-forensics.com>
2023-03-06 18:50:42 +0100

    remove irrelevant lines

--- main/hello-world.cc
+++ main/hello-world.cc
@@ -1,26 +1,11 @@
 #include <ctime>
 #include <string>
 #include <iostream>
-#include <openssl/err.h>
 #include <openssl/evp.h>
-#include <openssl/rand.h>
-
-std::string get_greet(const std::string& who) {
-  return "Hello " + who;
-}
-
-void print_localtime() {
-  std::time_t result = std::time(nullptr);
-  std::cout << std::asctime(std::localtime(&result));
-}
 
 int main(int argc, char** argv) {
-  std::string who = "world";
-  if (argc > 1) {
-    who = argv[1];
-  }
-  std::cout << get_greet(who) << std::endl;
-  print_localtime();
+  // Testing openssl
+  std::cout << "Testing OpenSSL:" << std::endl;
   cipher = EVP_get_cipherbyname("aes-256-cbc");
   EVP_add_cipher(cipher);
   return 0;
